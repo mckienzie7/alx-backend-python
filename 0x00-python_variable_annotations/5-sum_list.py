6fb592c29099f871b688761cf9ab771dc62d2bcb
Michael Solomon <Michaelelsa12@gmail.com>
2024-05-02 15:53:01 +0300

    list annotaion

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""complex floats. List with floats."""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum of values of list is returned"""
+    return sum(input_list)
