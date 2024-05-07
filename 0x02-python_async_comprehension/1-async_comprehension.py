1dc5b8cc6b8dea5ff1baf9bdfc9a183031a7d4a2
Michael Solomon <Michaelelsa12@gmail.com>
2024-05-08 00:06:28 +0300

    async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-
+"""Async Comprehension"""
 import asyncio
-
 async_generator = __import__('0-async_generator').async_generator
+from typing import List
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
+    """
+    A async comprehension coroutine that give random list,
+    By iterating through the async object
+    """
     result = [i async for i in async_generator()]
     return result
