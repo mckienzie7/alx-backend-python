1cebdb0df0c75c97850367f3b5d13e7c573c01c1
Michael Solomon <Michaelelsa12@gmail.com>
2024-05-07 23:56:17 +0300

    async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension():
+    result = [i async for i in async_generator()]
+    return result
